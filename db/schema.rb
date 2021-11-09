d0f4c08c54df1671b82c80b45a6899159dc02ce4
Alison Clare <alisonclare93@gmail.com>
2021-11-09 15:20:04 +0000

    add image url column to articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_09_151613) do
+ActiveRecord::Schema.define(version: 2021_11_09_151842) do
 
   create_table "articles", force: :cascade do |t|
     t.string "title"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 2021_11_09_151613) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.text "subheading"
+    t.string "image_url"
   end
 
 end
