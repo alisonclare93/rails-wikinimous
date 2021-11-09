610bcb8fd5eb1ec81886f58825918ade205f9c47
Alison Clare <alisonclare93@gmail.com>
2021-11-09 15:00:12 +0000

    seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+10.times do
+  article = Article.create(
+    title:    Faker::Marketing.buzzwords,
+    content:  Faker::Quotes::Shakespeare.as_you_like_it_quote,
+  )
+end
